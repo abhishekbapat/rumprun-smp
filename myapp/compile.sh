8fd40c635ac779b53045c942e247031f2f750e0e
abhishekbapat <abhishekbapat28@gmail.com>
2021-05-06 17:34:59 -0400

    chore: cleanup

--- myapp/compile.sh
+++ myapp/compile.sh
@@ -4,11 +4,6 @@ export PATH="${PATH}:$(pwd)/../rumprun-uefi_support/bin"
 
 (cd src && x86_64-rumprun-netbsd-gcc -Wall -O2 -o ../myapp main.c -DPORTMAP -DRUMPRUN)
 
-cp ../rumprun/rumprun-x86_64/lib/libutil.a ../rumprun/rumprun-x86_64/lib/rumprun-hw/
-cp ../rumprun/rumprun-x86_64/lib/libprop.a ../rumprun/rumprun-x86_64/lib/rumprun-hw/
-
-x86_64-rumprun-netbsd-cookfs -s 1 rootfs.fs rootfs
-
-rumprun-bake -m "add rootfs.fs" hw_generic myapp.bin myapp
+rumprun-bake hw_generic myapp.bin myapp
 
 sudo ./build_iso.sh
